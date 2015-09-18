90090fb09dd5b8ad69589099c52d5972cefb60de
jd615645 <jd615645@gmail.com>
2015-09-18 22:26:54 +0800

    :cat2:聊天室更新中

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  	res.sendfile('index.html');
+    res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
-	socket.broadcast.emit('hi');
-  	socket.on('disconnect', function(){
-  	    io.emit('chat message', "user disconnectgit");
-  	});
-  	socket.on('chat message', function(msg){
-    	io.emit('chat message', msg);
-  	});
+  socket.broadcast.emit('hi');
+    // socket.on('disconnect', function(){
+    //     io.emit('chat message', "user disconnect");
+    // });
+    socket.on('chat message', function(msg){
+      console.log(msg);
+      io.emit('chat message', msg);
+    });
 });
 
 http.listen(3000, function(){
-  	console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
 
 io.emit('some event', { for: 'everyone' });
